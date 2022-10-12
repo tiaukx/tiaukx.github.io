6071707742fd7ef2e1ff4cd7149e68020cf3eeea
tiaukx <47751992+tiaukx@users.noreply.github.com>
2022-10-12 13:06:30 +0100

    card generation working
    Still bugs surrounding handleClick

--- twilight_drinking_bingo/src/components/CardGrid.js
+++ twilight_drinking_bingo/src/components/CardGrid.js
@@ -7,59 +7,84 @@ import { Container, Row } from 'react-bootstrap';
 const CardGrid = () => {
 
     const [randomCards, setRandomCards] = useState([]);
-
-    useEffect(() => {
-       
-    }, [randomCards])
+    const [usedCards, setUsedCards] = useState([]);
+    const [counter, setCounter] = useState(0);
 
     const gridWidth = 3;
     const gridHeight = 3;
     const gridSize = gridWidth * gridHeight;
     let arrLength = 0;
 
-    const generateRandom = () => {
+    const tempArr = [...randomCards];
+    // let usedCards = [];
 
-        const temp = Math.floor(Math.random() * 64);
-        const newVal = cardsInfo[temp];
-        const inArray = randomCards.includes(cardsInfo[temp])
-        // let index = randomCards.indexOf(newVal)
+    useEffect(() => {
 
-        if (!inArray) {
-            setRandomCards(prev => [...prev, newVal]);
-            arrLength++;
+        const generateRandom = () => {
+            const temp = Math.floor(Math.random() * 10);
+            const newVal = cardsInfo[temp];
+            const inArray = tempArr.includes(cardsInfo[temp])
+            let index = tempArr.indexOf(newVal);
+
+            if (!inArray) {
+                tempArr.push(newVal);
+                arrLength++;
+                setRandomCards(tempArr);
+            } else if (inArray) {
+                tempArr.splice(index, 1);
+                arrLength--;
+                setRandomCards(tempArr);
+            } 
+        };
+
+        while (arrLength < gridSize) {
+            generateRandom();
         }
 
-        return randomCards, arrLength;
+    }, [])
+
+    const generateNewCard = (index) => {
+        const temp = Math.floor(Math.random() * 10);
+        const newVal = cardsInfo[temp];
+        const inArray = tempArr.includes(cardsInfo[temp])
+        const alreadyUsed = usedCards.includes(cardsInfo[temp]);
+
+        if (usedCards.length === cardsInfo.length - gridSize) {
+            setUsedCards([]);
+        } 
+
+        if (!inArray && !alreadyUsed) {
+            tempArr[index] = newVal;
+            arrLength++;
+            setRandomCards(tempArr)
+        } else {
+            generateNewCard();
+        } 
     };
-
-    // const removeDupes = (data) => {
-    //     return data.filter((val, index) => data.indexOf(val) === index);
-    // }
-
-    // removeDupes(randomCards);
-
-    do {
-        generateRandom();
-    } while (arrLength < gridSize)
 
     const handleClick = (cardId) => {
-        let chosenCard = randomCards.find(e => e.cardId === cardId);
-        let index = randomCards.indexOf(chosenCard);
+        let chosenCard = tempArr.find(e => e.cardId === cardId);
+        let index = tempArr.indexOf(chosenCard);
 
-        randomCards.splice(index, 1);
-        randomCards[index] = generateRandom();
+        if (!usedCards.includes(chosenCard)) {
+            setUsedCards(prev => [...prev, chosenCard]);
+        }
+        generateNewCard(index);
     };
+
+    console.log('Used Cards: ', usedCards)
 
     return (
         <>
 
-            <Container id='fullGrid' className="d-flex vw-100">
-                <Row className="m-auto">
-                    {randomCards.map(card => (
-                        <Card key={card.cardId} cardId={card.cardId} cardText={card.cardText} handleClick={handleClick} />
-                    ))}
-                </Row>
-            </Container>
+                <Container id='fullGrid' className="d-flex vw-100">
+                    <Row className="m-auto">
+                        {randomCards.map(card => (
+                            <Card key={card.cardId} cardId={card.cardId} cardText={card.cardText} handleClick={handleClick} />
+                        ))}
+                    </Row>
+                </Container>
+
         </>
     )
 };
